a07431be0c0887d62e582913da3310470c727ffa
josh k <github.contact@jpa.anonaddy.com>
2024-01-09 00:24:55 -0500

    Use relative path of project images
    Used relative path of project images instead of importing images.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,19 +1,15 @@
 import ProjectCard from "./ProjectCard.jsx";
-import debianFrontendImage from "./assets/debian-front-end-sample-image.png";
-import videoSharingWebpageImage from "./assets/generic-video-sharing-webpage-image-sample.png";
-import calculatorProjectImage from "./assets/calculator-image-sample.png";
-import gpaCalculatorImage from "./assets/houghton-gpa-calculator-sample-image.png";
 
 export default function Projects() {
   const debianFrontend = {
-    thumbnail: debianFrontendImage,
+    thumbnail: "./src/assets/debian-front-end-sample-image.png",
     title: "Debian Mockup Front-end",
     description:
       "A mockup webpage of Debian.org that intends to explore improvements in layout, aesthetics, user-friendliness, and minimalism.",
     url: "https://github.com/jpga1/debian-mockup-front-end",
   };
 
-  const videoSharingWebpage = {
+  const portfolioWebpage = {
     thumbnail: "./src/assets/portfolio-site-image.png",
     title: "Portfolio Webpage",
     description:
@@ -22,7 +18,7 @@ export default function Projects() {
   };
 
   const calculator = {
-    thumbnail: calculatorProjectImage,
+    thumbnail: "./src/assets/calculator-image-sample.png",
     title: "Calculator",
     description:
       "A calculator web application that performs fundamental arthimetic functions.",
@@ -30,7 +26,7 @@ export default function Projects() {
   };
 
   const gpaCalculator = {
-    thumbnail: gpaCalculatorImage,
+    thumbnail: "./src/assets/houghton-gpa-calculator-sample-image.png",
     title: "Grade Point Average Calculator",
     description:
       "Calculates the current semester grade point average and overall grade point average based on current semester credits, cummulative credits, and cummulative grade point average data. Credit and grading schemes are based on Houghton University's standards",
@@ -41,7 +37,7 @@ export default function Projects() {
     <div className="container-fluid text-white text-center p-5">
       <h2 className="display-4 mb-5">Projects</h2>
       <ProjectCard {...debianFrontend} />
-      <ProjectCard {...videoSharingWebpage} />
+      <ProjectCard {...portfolioWebpage} />
       <ProjectCard {...calculator} />
       <ProjectCard {...gpaCalculator} />
     </div>
