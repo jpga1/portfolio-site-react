8db3ed8b79c5009ad4f1e81114e4c48c86d4e3e5
josh k <github.contact@jpa.anonaddy.com>
2024-01-24 03:25:59 -0500

    Fix project image centering on smaller screens
    Remove right auto margins and justified content around.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 export default function ProjectCard({ thumbnail, title, description, url }) {
   return (
-    <div className="d-flex flex-wrap w-100 justify-content-center mb-5 project bg-light p-5 text-dark">
-      <div className="project-image-container mr-auto">
+    <div className="d-flex flex-wrap w-100 justify-content-around mb-5 project bg-light p-5 text-dark">
+      <div className="project-image-container">
         <img className="project-thumbnail" src={thumbnail} alt="" />
       </div>
       <div className="w-50">
